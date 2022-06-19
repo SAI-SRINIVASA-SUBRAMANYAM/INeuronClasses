cdbff6542ae77d35bdba97b7426c19cad7aceb80
SUBRAMANYAM <subramanyam.sista@gmail.com>
2022-06-19 22:24:54 +0530

    Added get prime numbers program

---
+++ INeuronPracticeSessions/Tasks/June_2022/June052022/GetPrimeNumbers.py
@@ -0,0 +1,28 @@
+def is_prime_number(num: int):
+    '''Validate whether give number is prime or not!'''
+    m = num // 2 + 1
+    for j in range(2, m):
+        if (num % j == 0):
+            return False
+    return True
+
+
+def find_prime_numbers(n: int):
+    '''Find the prime number 1 to n numbers'''
+    if type(n) != int:
+        raise Exception('Invalid input, '+str(n))
+    elif n <= 1:
+        yield None
+    elif n == 2:
+        yield 2
+    else:
+        for i in range(2, n + 1):
+            if (is_prime_number(i)):
+                yield i
+
+if __name__ == '__main__':
+    try:
+        for i in find_prime_numbers(10):
+            if i: print(i, end=", ")
+    except Exception as e:
+        print(e)
